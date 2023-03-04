8a56473d15c55ac532999a9ac643ea267b98c532
guillaume_pc <guillaume.m78@hotmail.fr>
2023-03-04 20:42:38 +0100

    delt : suppression du fichier datatest et de recupdata.py

--- connexion_mysql_server.py
+++ connexion_mysql_server.py
@@ -1,59 +1,54 @@
-import mysql.connector as MC
 from Data.database_handler import DatabaseHandler
 
-class DatabaseHandler():
-	def __init__(self, database_name : str):
-		conn = MC.connect(host = 'localhost', database = database_name, user = 'root', password = '')
-		cursor = conn.cursor()
+database_handler = DatabaseHandler()
 
-	def create_person(self, username: str, password: str):
-		req = 'INSERT INTO `person`(`username`, `password`) VALUES (%s,%s);'
-		info =(username, password)
-		cursor.execute(req, info)
-		conn.commit()
-
-	def password_for(self, username: str):
-		cursor = conn.cursor()
-		req = f"SELECT password FROM Person WHERE username = ?;"
-		cursor.execute(req, (username,))
-		result = cursor.fetchall()
-		cursor.close()
-		return dict(result[0])["password"]
-
-	def user_exists_with(self, username: str) -> bool:
-		cursor = conn.cursor()
-		req = f"SELECT * FROM Person WHERE username = ?;"
-		cursor.execute(req, (username,))
-		result = cursor.fetchall()
-		cursor.close()
-
-		return len(result) == 1
-
-try :
-	conn = MC.connect(host = 'localhost', database = 'datatest', user = 'root', password = '')
-	cursor = conn.cursor()
-
+def register():
 	print("---Register---")
 	username = input("Username : ")
 	password = input("Mot de passe : ")
 
+	database_handler.create_person(username, password)
+
+def modif():
+	print("---Edit---")
+	username = input("Username : ")
+	password = input("Mot de passe a modifier : ")
+
+	database_handler.password_edit(username, password)
 
 
-	req = 'INSERT INTO `person`(`username`, `password`) VALUES (%s,%s);'
-	info =(username, password)
+def login():
+	print("---Login---")
+	username = input("Username : ")
+	password = input("Mot de passe : ")
+	verif = "('"+password+"',)"
 
-	cursor.execute(req, info)
-	conn.commit()
+	if database_handler.user_exists_with(username) and verif == database_handler.password_for(username):
+		#menu_connected()
+		print("login")
+	else:
+		print("Nom d'utilisateur/mot de passe incorrect.")
+		print(database_handler.password_for(username))
 
-	req = 'SELECT * FROM person'
-	cursor.execute(req)
+def menu_not_connect():
+	while True:
+		print("Bienvenue !")
+		print("Choisissez une option")
+		print("1. Login")
+		print("2. Register")
+		print("3. Edit")
+		print("4. Exit")
+		choix = int(input())
 
-	personlist = cursor.fetchall()
+		if choix == 1:
+			login()
 
-	for i in personlist:
-		print('Prénom : {}'.format(i[0]))
-		print('Mdp : {}'.format(i[1]))
+		if choix == 2:
+			register()
 
-finally:
-	cursor.close()
-	conn.close()
+		if choix == 3:
+			modif()
+		if choix == 4:
+			database_handler.stop()
+
+menu_not_connect()
