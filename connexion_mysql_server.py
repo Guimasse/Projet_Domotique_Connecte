c05b3731afc48d70360039649d367c45bdcfae2e
guillaume_pc <guillaume.m78@hotmail.fr>
2023-03-04 20:39:08 +0100

    feat : ajout de la fonctionnalité de modif dans la bd et ajout connection a bd distante

--- connexion_mysql_server.py
+++ connexion_mysql_server.py
@@ -1,4 +1,33 @@
 import mysql.connector as MC
+from Data.database_handler import DatabaseHandler
+
+class DatabaseHandler():
+	def __init__(self, database_name : str):
+		conn = MC.connect(host = 'localhost', database = database_name, user = 'root', password = '')
+		cursor = conn.cursor()
+
+	def create_person(self, username: str, password: str):
+		req = 'INSERT INTO `person`(`username`, `password`) VALUES (%s,%s);'
+		info =(username, password)
+		cursor.execute(req, info)
+		conn.commit()
+
+	def password_for(self, username: str):
+		cursor = conn.cursor()
+		req = f"SELECT password FROM Person WHERE username = ?;"
+		cursor.execute(req, (username,))
+		result = cursor.fetchall()
+		cursor.close()
+		return dict(result[0])["password"]
+
+	def user_exists_with(self, username: str) -> bool:
+		cursor = conn.cursor()
+		req = f"SELECT * FROM Person WHERE username = ?;"
+		cursor.execute(req, (username,))
+		result = cursor.fetchall()
+		cursor.close()
+
+		return len(result) == 1
 
 try :
 	conn = MC.connect(host = 'localhost', database = 'datatest', user = 'root', password = '')
--- recupdata.py
+++ recupdata.py
@@ -1,6 +1,6 @@
 from Data.database_handler import DatabaseHandler
 
-database_handler = DatabaseHandler("database.db")
+database_handler = DatabaseHandler()
 
 def register():
 	print("---Register---")
@@ -9,16 +9,26 @@ def register():
 
 	database_handler.create_person(username, password)
 
+def modif():
+	print("---Edit---")
+	username = input("Username : ")
+	password = input("Mot de passe a modifier : ")
+
+	database_handler.password_edit(username, password)
+
+
 def login():
 	print("---Login---")
 	username = input("Username : ")
 	password = input("Mot de passe : ")
+	verif = "('"+password+"',)"
 
-	if database_handler.user_exists_with(username) and password == database_handler.password_for(username):
+	if database_handler.user_exists_with(username) and verif == database_handler.password_for(username):
 		#menu_connected()
 		print("login")
 	else:
 		print("Nom d'utilisateur/mot de passe incorrect.")
+		print(database_handler.password_for(username))
 
 def menu_not_connect():
 	while True:
@@ -26,6 +36,8 @@ def menu_not_connect():
 		print("Choisissez une option")
 		print("1. Login")
 		print("2. Register")
+		print("3. Edit")
+		print("4. Exit")
 		choix = int(input())
 
 		if choix == 1:
@@ -33,5 +45,10 @@ def menu_not_connect():
 
 		if choix == 2:
 			register()
+
+		if choix == 3:
+			modif()
+		if choix == 4:
+			database_handler.stop()
 
 menu_not_connect()
